f775d313748a8fbb285b18aeff87219cd0b13ba0
Adewole Adewumi <wolejr@gmail.com>
2024-07-31 11:33:56 -0400

    Changed the title to Customers

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
         
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         
-        title: Text(widget.title),
+        title: const Text("Customers"),
       ),
       body: Center(
         
